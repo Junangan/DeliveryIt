2eea270110eb5783beb552ba734a617a0ef6435e
GraceFongChingLin <55775141+GraceFongChingLin@users.noreply.github.com>
2022-11-15 02:22:08 +0800

    Add files via upload

--- DeliveryIt/deliveryit.sql
+++ DeliveryIt/deliveryit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 03, 2022 at 09:33 PM
+-- Generation Time: Nov 14, 2022 at 07:21 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -30,17 +30,39 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `favourite` (
   `FavouriteID` int(255) NOT NULL,
-  `FoodOrDrink` text NOT NULL
+  `FoodOrDrinkName` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `favourite`
 --
 
-INSERT INTO `favourite` (`FavouriteID`, `FoodOrDrink`) VALUES
-(13, '2-pc Chicken'),
-(15, 'Manhattan Fish and Chips'),
-(16, 'Caramel Macchiato');
+INSERT INTO `favourite` (`FavouriteID`, `FoodOrDrinkName`) VALUES
+(29, 'Chicken'),
+(30, 'Pizza');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foodanddrink`
+--
+
+CREATE TABLE `foodanddrink` (
+  `FoodId` int(255) NOT NULL,
+  `FoodOrDrinkName` text NOT NULL,
+  `Price` double(10,2) NOT NULL,
+  `RestaurantId` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `foodanddrink`
+--
+
+INSERT INTO `foodanddrink` (`FoodId`, `FoodOrDrinkName`, `Price`, `RestaurantId`) VALUES
+(28, 'Chicken', 5.00, 19),
+(29, 'Milo', 1.00, 19),
+(30, 'Spaghetti', 20.00, 20),
+(31, 'Pizza', 20.00, 20);
 
 -- --------------------------------------------------------
 
@@ -49,20 +71,23 @@ INSERT INTO `favourite` (`FavouriteID`, `FoodOrDrink`) VALUES
 --
 
 CREATE TABLE `owner` (
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `Username` text NOT NULL,
+  `Password` text NOT NULL,
+  `Fullname` text NOT NULL,
+  `Email` text NOT NULL,
+  `Phone` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `owner`
 --
 
-INSERT INTO `owner` (`username`, `password`) VALUES
-('Evelyn', 'evelyn'),
-('Felix', 'felix'),
-('Fong Keng Lun', 'fkl'),
-('Grace', 'grace'),
-('Lucille', 'lucille');
+INSERT INTO `owner` (`Username`, `Password`, `Fullname`, `Email`, `Phone`) VALUES
+('Grace', 'grace', 'Grace Fong Ching Lin', 'gracefongcl@gmail.com', 132091628),
+('Evelyn', 'evelyn', '', '', 0),
+('Lucille', 'lucille', '', '', 0),
+('Fong Keng Lun', 'fkl', '', '', 0),
+('Murphy', 'murphy', '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -71,21 +96,20 @@ INSERT INTO `owner` (`username`, `password`) VALUES
 --
 
 CREATE TABLE `restaurant` (
-  `RestaurantID` int(255) NOT NULL,
-  `RestaurantName` varchar(255) NOT NULL,
-  `FoodOrDrink` varchar(255) NOT NULL,
-  `Price` varchar(255) NOT NULL
+  `RestaurantId` int(255) NOT NULL,
+  `RestaurantName` text NOT NULL,
+  `ratingNumber` int(255) NOT NULL,
+  `star` int(255) NOT NULL,
+  `Image` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `restaurant`
 --
 
-INSERT INTO `restaurant` (`RestaurantID`, `RestaurantName`, `FoodOrDrink`, `Price`) VALUES
-(4, 'KFC', '2-pc Chicken', '11.99'),
-(5, 'Manhattan Fish Market', 'Manhattan Fish and Chips', '22.90'),
-(6, 'Starbucks', 'Caramel Macchiato', '1.00'),
-(12, 'Pizza Hut', 'Deluxe Cheese', '10.00');
+INSERT INTO `restaurant` (`RestaurantId`, `RestaurantName`, `ratingNumber`, `star`, `Image`) VALUES
+(19, 'KFC', 0, 0, 0x6b66632e706e67),
+(20, 'Pizza Hut', 0, 0, 0x70697a7a616875742e706e67);
 
 -- --------------------------------------------------------
 
@@ -94,19 +118,44 @@ INSERT INTO `restaurant` (`RestaurantID`, `RestaurantName`, `FoodOrDrink`, `Pric
 --
 
 CREATE TABLE `user` (
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `Fullname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` text NOT NULL
+  `Username` text NOT NULL,
+  `Password` text NOT NULL,
+  `Fullname` text NOT NULL,
+  `Email` text NOT NULL,
+  `Phone` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `password`, `Fullname`, `email`, `phone`) VALUES
-('Grace', 'grace', 'Grace Fong Ching Lin', 'gracefongcl@gmail.com', '0132091628');
+INSERT INTO `user` (`Username`, `Password`, `Fullname`, `Email`, `Phone`) VALUES
+('Grace', 'grace', 'Grace Fong Ching Lin', 'gracefongcl@gmail.com', 132091628),
+('Evelyn', 'evelyn', 'Evelyn Fong Ching Har', 'evelynfong@hotmail.com', 123456789);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userorder`
+--
+
+CREATE TABLE `userorder` (
+  `OrderId` int(255) NOT NULL,
+  `RestaurantName` text NOT NULL,
+  `FoodName` text NOT NULL,
+  `FoodNumber` int(255) NOT NULL,
+  `TotalPrice` double(10,2) NOT NULL,
+  `Status` text NOT NULL,
+  `user` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userorder`
+--
+
+INSERT INTO `userorder` (`OrderId`, `RestaurantName`, `FoodName`, `FoodNumber`, `TotalPrice`, `Status`, `user`) VALUES
+(14, 'KFC', 'Chicken', 1, 5.00, 'pending', 'Grace'),
+(15, 'Pizza Hut', 'Pizza', 1, 20.00, 'pending', 'Grace');
 
 --
 -- Indexes for dumped tables
@@ -119,22 +168,22 @@ ALTER TABLE `favourite`
   ADD PRIMARY KEY (`FavouriteID`);
 
 --
--- Indexes for table `owner`
+-- Indexes for table `foodanddrink`
 --
-ALTER TABLE `owner`
-  ADD PRIMARY KEY (`username`);
+ALTER TABLE `foodanddrink`
+  ADD PRIMARY KEY (`FoodId`);
 
 --
 -- Indexes for table `restaurant`
 --
 ALTER TABLE `restaurant`
-  ADD PRIMARY KEY (`RestaurantID`);
+  ADD PRIMARY KEY (`RestaurantId`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `userorder`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`username`);
+ALTER TABLE `userorder`
+  ADD PRIMARY KEY (`OrderId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -144,13 +193,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `favourite`
 --
 ALTER TABLE `favourite`
-  MODIFY `FavouriteID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `FavouriteID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `foodanddrink`
+--
+ALTER TABLE `foodanddrink`
+  MODIFY `FoodId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `restaurant`
 --
 ALTER TABLE `restaurant`
-  MODIFY `RestaurantID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `RestaurantId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `userorder`
+--
+ALTER TABLE `userorder`
+  MODIFY `OrderId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
