be2a9aa7697f2c9231ceb716bb40e2a4c5c44a28
Gan Jun An <junangan00@gmail.com>
2022-11-17 17:37:46 +0800

    Update favourite.php

--- DeliveryIt/PHP/favourite.php
+++ DeliveryIt/PHP/favourite.php
@@ -34,9 +34,16 @@
     $foodDrink = $_POST["foodDrink"];
     $price;
 
-    $CheckOwnerQuery = "SELECT * from favourite where FoodOrDrinkName='$foodDrink'";
-
-    $result = $con->query($CheckOwnerQuery);
+    try{
+      $CheckOwnerQuery = "SELECT * from favourite where FoodOrDrinkName='$foodDrink'";
+      $result = $con->query($CheckOwnerQuery);
+    }
+    catch(Exception $e){
+      $CreateFavouriteQuery= "CREATE TABLE favourite (FavouriteID integer NOT NULL AUTO_INCREMENT PRIMARY KEY,FoodOrDrinkName integer(255) NOT NULL)";
+      $con->query($CreateFavouriteQuery);
+      $CheckOwnerQuery = "SELECT * from favourite where FoodOrDrinkName='$foodDrink'";
+      $result = $con->query($CheckOwnerQuery);
+    }
         if($result->num_rows > 0){
           echo "<script>
           alert('The name of the food or drink is duplicate , please choose another food or drink');
