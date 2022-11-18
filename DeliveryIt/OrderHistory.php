bb352758525ac0f2442b707e7c83836175707e02
Gan Jun An <junangan00@gmail.com>
2022-11-18 20:12:31 +0800

    Update OrderHistory.php

--- DeliveryIt/OrderHistory.php
+++ DeliveryIt/OrderHistory.php
@@ -23,7 +23,7 @@ if(isset($_POST['submit'])){
   $GetUserOrderQuery = "SELECT * from userorder WHERE OrderId='$Id'";
   $result = $conn->query($GetUserOrderQuery);
   $row = $result->fetch_assoc();
-    $UpdateOrderQuery = "UPDATE userorder SET Rating='$Star'";
+    $UpdateOrderQuery = "UPDATE userorder SET Rating='$Star' WHERE OrderId='$Id'";
     $conn->query($UpdateOrderQuery);
   echo "<script>
   alert('Change Successfully');
@@ -52,6 +52,7 @@ if(isset($_POST['submit'])){
     margin: 0;
     padding: 0;
 	}
+
 	.rate {
 	    float: left;
 	    height: 46px;
@@ -74,12 +75,12 @@ if(isset($_POST['submit'])){
 	    content: '★ ';
 	}
 	.rate > input:checked ~ label {
-	    color: #ffc700;
+	    color: #ffc700;    
 	}
 
 	.rate:not(:checked) > label:hover,
 	.rate:not(:checked) > label:hover ~ label {
-	    color: #deb217;
+	    color: #deb217;  
 	}
 	.rate > input:checked + label:hover,
 	.rate > input:checked + label:hover ~ label,
@@ -102,7 +103,7 @@ if(isset($_POST['submit'])){
             </div>
             <ul class = "nav navbar-nav navbar-left">
               <li class = "nav-item"><a class = "nav-link" href = "UserPage.php"> Home </a></li>
-              <li class = "nav-item"><a class = "nav-link" href = "restaurant.php"> Restaurant </a></li>
+              <li class = "nav-item"><a class = "nav-link" href = "Restaurant.php"> Restaurant </a></li>
               <li class = "nav-item"><a class = "nav-link" href = "FavouriteList.php"> Favourite List </a></li>
               <li class = "nav-item"><a class = "nav-link" href = "#"> View All Order History </a></li>
               <li class = "nav-item"><a class = "nav-link" href = "ManageUserProfile.php"> Manage Profile </a></li>
@@ -118,28 +119,29 @@ if(isset($_POST['submit'])){
     	<div class="row container-fluid justify-content-center">
 	      <h3>All order History</h3>
 	    </div>
-	    <div class="container">
-        <div id="allRestaurant" class="row justify-content-between">
-          <div class = "container">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Restaurant Name</th>
-              <th scope="col">Food Name</th>
-              <th scope="col">Number Order</th>
-              <th scope="col">Total price</th>
-              <th scope="col">Status</th>
-              <th scope="col">Rating</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
             <?php
             //Step 3: Display result
             $con = new mysqli ('localhost', 'root', '',"DeliveryIt");
+            try{
                 $GetRestaurantQuery = "SELECT * from userorder";
                 $result = $con->query($GetRestaurantQuery);
                 if (mysqli_num_rows($result) > 0) {
+                	echo "<div class='container'>
+                          <div id='allRestaurant' class='row justify-content-between'>
+                            <div class = 'container'>
+                          <table class='table'>
+                            <thead>
+                              <tr>
+                                <th scope='col'>Restaurant Name</th>
+                                <th scope='col'>Food Name</th>
+                                <th scope='col'>Number Order</th>
+                                <th scope='col'>Total price</th>
+                                <th scope='col'>Status</th>
+                                <th scope='col'>Rating</th>
+                                <th scope='col'></th>
+                              </tr>
+                            </thead>
+                            <tbody>";
                   while($row = mysqli_fetch_assoc($result)) {
                     echo "<tr>
                             <td>{$row['RestaurantName']}</td>
@@ -148,20 +150,19 @@ if(isset($_POST['submit'])){
                             <td>{$row['TotalPrice']}</td>
                             <td>{$row['Status']}</td>";
 
-                      if($row['Rating']==null){
-                        echo "<td><div class='rate' id='{$row['Rating']}'>
-                      }
-                  <input type='radio' id='star5' name='rate' value='5' />
-                  <label for='star5' title='text'>5 stars</label>
-                  <input type='radio' id='star4' name='rate' value='4' />
-                  <label for='star4' title='text'>4 stars</label>
-                  <input type='radio' id='star3' name='rate' value='3' />
-                  <label for='star3' title='text'>3 stars</label>
-                  <input type='radio' id='star2' name='rate' value='2' />
-                  <label for='star2' title='text'>2 stars</label>
-                  <input type='radio' id='star1' name='rate' value='1' />
-                  <label for='star1' title='text'>1 star</label>
-                </div></td>
+                      if($row['Rating']==0 && $row['Status']!='pending'){
+                        echo "<td><div class='rate' id='rate{$row['OrderId']}'>
+			                  <input type='radio' id='{$row['OrderId']}star5' name='rate{$row['OrderId']}' value='5' />
+			                  <label for='{$row['OrderId']}star5' title='text'>5 stars</label>
+			                  <input type='radio' id='{$row['OrderId']}star4' name='rate{$row['OrderId']}' value='4' />
+			                  <label for='{$row['OrderId']}star4' title='text'>4 stars</label>
+			                  <input type='radio' id='{$row['OrderId']}star3' name='rate{$row['OrderId']}' value='3' />
+			                  <label for='{$row['OrderId']}star3' title='text'>3 stars</label>
+			                  <input type='radio' id='{$row['OrderId']}star2' name='rate{$row['OrderId']}' value='2' />
+			                  <label for='{$row['OrderId']}star2' title='text'>2 stars</label>
+			                  <input type='radio' id='{$row['OrderId']}star1' name='rate{$row['OrderId']}' value='1' />
+			                  <label for='{$row['OrderId']}star1' title='text'>1 star</label>
+			                </div></td>
                             <td>
                                 <form action='' method='Post' onsubmit='return checkStar({$row['OrderId']})' enctype='multipart/form-data'>
                                 <input type='text' name='id'  value='{$row['OrderId']}' style='Display:none;'>
@@ -171,14 +172,20 @@ if(isset($_POST['submit'])){
                                 </td>
                           </tr>";
                       }
+                      else if($row['Status']=='pending'){
+                        echo "";
+                      }
                       else{
                         echo "<td>You Rating {$row['Rating']} star</td>";
                       }
                   }
                 }
-                else {
-                  echo"<p>No history</p>";
-                }
+            }
+            catch(Exception $e){
+                echo "<p style='text-align:center; padding-top:2.5%;'>
+                          No any history
+                        </p>";
+              }
             ?>
           </tbody>
       </table>
@@ -193,19 +200,19 @@ if(isset($_POST['submit'])){
     </div>
     <script type="text/javascript">
       function checkStar(id){
-        if (document.getElementById('star5').checked) {
+        if (document.getElementById(id+'star5').checked) {
           document.getElementById(id).value = 5;
         }
-        else if(document.getElementById('star4').checked){
+        else if(document.getElementById(id+'star4').checked){
           document.getElementById(id).value = 4;
         }
-        else if(document.getElementById('star3').checked){
+        else if(document.getElementById(id+'star3').checked){
           document.getElementById(id).value = 3;
         }
-        else if(document.getElementById('star2').checked){
+        else if(document.getElementById(id+'star2').checked){
           document.getElementById(id).value = 2;
         }
-        else if(document.getElementById('star1').checked){
+        else if(document.getElementById(id+'star1').checked){
           document.getElementById(id).value = 1;
         }
         else{
